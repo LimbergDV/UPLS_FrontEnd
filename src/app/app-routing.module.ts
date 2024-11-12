0b18ce26f6095dc5be75951b77107cfad0bda4d9
Alexis Guzman <spartanthelexvro@gmail.com>
2024-11-11 21:05:39 -0600

    Añadiendo mapa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { FormLoginComponent } from './login/form-login/form-login.component';
 import { ViewProfileComponent } from './profile/view-profile/view-profile.component';
 import { FormRegisterComponent } from './register/form-register/form-register.component';
 import { HomeComponent } from './home/home.component';
+import { MapComponent } from './maps/map/map.component';
 
 const routes: Routes = [
   { path: 'signIn', component: FormLoginComponent },
   { path: 'profile', component: ViewProfileComponent },
   { path: 'signUp', component: FormRegisterComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'maps', component: MapComponent },
 ];
 
 @NgModule({
