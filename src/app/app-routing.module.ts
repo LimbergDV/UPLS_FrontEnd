375d999f6867fa8d3bb2e3a9920a4f8880ad31a5
LimbergDV <limbergdelvalle@gmail.com>
2024-11-11 20:50:04 -0600

    add: add publications finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { FormLoginComponent } from './login/form-login/form-login.component';
 import { ViewProfileComponent } from './profile/view-profile/view-profile.component';
 import { FormRegisterComponent } from './register/form-register/form-register.component';
 import { HomeComponent } from './home/home.component';
+import { AddPublicationComponent } from './publications/add-publication/add-publication.component';
+import { ViewPublicationComponent } from './publications/view-publication/view-publication.component';
 
 const routes: Routes = [
   { path: 'signIn', component: FormLoginComponent },
   { path: 'profile', component: ViewProfileComponent },
   { path: 'signUp', component: FormRegisterComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'publications', component: ViewPublicationComponent},
 ];
 
 @NgModule({
