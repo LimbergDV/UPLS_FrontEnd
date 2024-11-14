078b6472b79fd76ea9470fdcbe31513b7902a826
LimbergDV <limbergdelvalle@gmail.com>
2024-11-13 23:00:50 -0600

    add: trying changes in publications

--- src/app/publications/add-publication/add-publication.component.ts
+++ src/app/publications/add-publication/add-publication.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PublicationService } from '../../service/publications.service';
+import { IPublications } from '../../models/i-publications';
 
 @Component({
   selector: 'app-add-publication',
   templateUrl: './add-publication.component.html',
   styleUrl: './add-publication.component.css'
 })
-export class AddPublicationComponent {
 
+export class AddPublicationComponent {
+  title = '';
+  description = '';
+  image: File | null = null; // almacenar la imagen
+  date_limit = '';
+  blood_type = ''; // valor por defecto
+  donors_number = 0;
+
+  constructor(
+    private publicationService: PublicationService,
+    public router: Router
+  ) {}
+
+  // Método para manejar la carga de la imagen
+  onFileSelected(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      this.image = file;
+    }
+  }
+
+  // Método para agregar una publicación
+  onSubmit(): void {
+    // Preparamos el objeto con los datos del formulario
+    const newPublication: IPublications = {
+      id_donee: 1, // Generar un ID temporal
+      title: this.title,
+      description: this.description,
+      image: this.image ? this.image.name : '', 
+      date_limit: new Date(this.date_limit),
+      blood_type: '',
+    };
+
+    // Llamada al servicio para enviar la publicación al backend
+    this.publicationService.addPublication(newPublication).subscribe({
+      next: (response) => {
+        console.log('Publicación agregada:', response);
+        this.router.navigate(['/publications']); // Navegar a la lista de publicaciones
+      },
+      error: (error) => {
+        console.error('Error al agregar publicación:', error);
+      },
+    });
+  }
 }
